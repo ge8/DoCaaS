e17bc8c4b72e5bb62e0edb6062ff2cf2fd478653
jrs4 <jrs4>
2018-09-18 22:00:28 +1000

    With Background

--- docaas-react/src/App.js
+++ docaas-react/src/App.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Button } from 'reactstrap';
+import PropTypes from 'prop-types';
+import logo from './images/logo.png';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};    
+  }
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Deck Of Cards As A Service</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Button color="primary">Create</Button>{''}
       </div>
     );
   }
+}
+
+Button.propTypes = {
+  active: PropTypes.bool,
+  block: PropTypes.bool,
+  color: PropTypes.string, // default: 'secondary'
+  disabled: PropTypes.bool,
+
+  // Pass in a Component to override default button element
+  // example: react-router Link
+  // default: 'button'
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+
+  // ref will only get you a reference to the Button component, use innerRef to get a reference to the DOM element (for things like focus management).
+  innerRef: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+
+  onClick: PropTypes.func,
+  size: PropTypes.string
 }
 
 export default App;
