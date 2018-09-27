2b456f6a4e93e6cc3fa47cfe560172efd51d07a8
jrs4 <jrs4>
2018-09-27 21:40:59 +1000

    APIs

--- docaas-react/src/components/MainBody/MainBody.js
+++ docaas-react/src/components/MainBody/MainBody.js
@@ -7,6 +7,8 @@ const NOT_LOGED = 0
 const LOGING = 1
 const LOGED = 2
 
+const mainUrl = "http://estaba.net"
+
 let logingPage = null;
 let controls = null;
 let table = null;
@@ -15,9 +17,11 @@ let fixedDeck = { id:"", cards:[] };
 let prefixes = [ "C", "S", "D", "H" ];  // Spades, Clubs, Diamons, Hearts
 let cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" ];
 
+let counter = 0;
+
 prefixes.forEach(prefix => {
   cards.forEach( card => {
-    fixedDeck.cards.push(card+prefix+'.png');
+    fixedDeck.cards.push('Blank.png');
   })
 });
 
@@ -27,6 +31,7 @@ class MainBody extends React.Component {
     this.state = {
       'username': "",
       'password': "",
+      'message': "",
       'deck': fixedDeck
     };  
     this.handleLoged = this.handleLoged.bind(this);
@@ -47,22 +52,74 @@ class MainBody extends React.Component {
 
   handleCreate() {
     console.log('API for Create');
-    const response = this.callAPI('estaba.net/create');
+    const response = this.callAPI(mainUrl + '/create');
+    counter++;
+
+    fixedDeck.cards = [];
+    prefixes.forEach(prefix => {
+      cards.forEach( card => {
+        fixedDeck.cards.push('Blank.png');
+      })
+    });
+
+    this.setState({
+      'message': "Deck " + counter + " created!",
+      'deck': fixedDeck
+    });
   }
   
   handleGet() {
     console.log('API for Get');
-    const response = this.callAPI('estaba.net/get');
+    console.log(fixedDeck.cards);
+    const response = this.callAPI(mainUrl + '/get');
+    
+    fixedDeck.cards = [];
+    prefixes.forEach(prefix => {
+      cards.forEach( card => {
+        fixedDeck.cards.push(card+prefix+'.png');
+      })
+    });
+
+    this.setState({
+      'message': "",
+      'deck': fixedDeck
+    });
   }
 
   handleDeal() {
     console.log('API for Deal');
-    const response = this.callAPI('estaba.net/deal');
+    const response = this.callAPI(mainUrl + '/deal');
+    let dealDeck = this.state.deck;
+    let newStack = [];
+    this.state.deck.cards.map((object,i) => {
+      if (i!==4 && i!==6 && i!==20 && i!==22) {
+        newStack.push("Blank.png");
+      } else {
+        newStack.push(object);
+      }
+    });
+    dealDeck.cards = newStack;
+    this.setState({
+      'message': "",
+      'deck': dealDeck
+    });
   }
 
   handleShuffle() {
     console.log('API for Shuffle');
-    const response = this.callAPI('estaba.net/shuffle');
+    const response = this.callAPI(mainUrl + '/shuffle');
+    
+    fixedDeck.cards = [];
+    prefixes.forEach(prefix => {
+      cards.forEach( card => {
+        fixedDeck.cards.push(card+prefix+'.png');
+      })
+    });
+
+    this.setState({
+      'message': "",
+      'deck': fixedDeck
+    });
   }
 
   callAPI(url) {    
@@ -70,6 +127,7 @@ class MainBody extends React.Component {
       method: 'GET',
       headers: { Authorization: 'Basic ' + btoa(this.state.username + ":" + this.state.password) }
     }).then(response => {
+        console.log(response);
         if (response.ok) {
           return response.json();
         }
@@ -92,7 +150,7 @@ class MainBody extends React.Component {
                            handleGet={this.handleGet} _
                            handleDeal={this.handleDeal} _
                            handleShuffle={this.handleShuffle} />;
-      table = <Table deck={this.state.deck} />;
+      table = <Table deck={this.state.deck} message={this.state.message} />;
     }
 
     return (
