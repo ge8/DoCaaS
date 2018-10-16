77d47fa1c649d67060841633cc864f9d6e27f183
Estaba <grestaba@9801a79671c3.ant.amazon.com>
2018-10-17 09:24:38 +1100

    fix app publish

--- demos/setup.sh
+++ demos/setup.sh
@@ -42,9 +42,10 @@ echo "Adding record to R53"
 ZONEID=`aws route53 list-hosted-zones-by-name --dns-name $DOMAIN | jq --raw-output '.HostedZones[0].Id'`
 echo "ZONEID is $ZONEID"
 cd ../../../../../demos
-find r53c1.json -type f -exec sed -i -e "s/##TARGETGOESHERE##/$CNAMEC1/g" {} \;
+find r53c1.json -type f -exec sed -i -e "s/##TARGETGOESHERE##/https:\/\/$BUCKETC1.s3-website-us-west-2.amazonaws.com/g" {} \;
 find r53c1.json -type f -exec sed -i -e "s/##DOMAINGOESHERE##/$DOMAIN/g" {} \;
 aws route53 change-resource-record-sets --hosted-zone-id $ZONEID --change-batch file://r53c1.json 
 echo "Record to R53 Added"
 
 echo "Customer 1 url is: customer1.$DOMAIN"
+
--- demos/testscript.sh
+++ demos/testscript.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-# CNAMEC1=`aws elasticbeanstalk describe-environments --environment-names prodA | jq --raw-output '.Environments[0].CNAME'`
-# echo $CNAMEC1
-DOMAIN=estaba.net
-
-ZONEID=`aws route53 list-hosted-zones-by-name --dns-name $DOMAIN | jq --raw-output '.HostedZones[0].Id'`
-echo $ZONEID
 
 
-find r53.json -type f -exec sed -i -e "s/##SUBDOMAINGOESHERE##/customer1/g" {} \;
-find r53.json -type f -exec sed -i -e "s/##DOMAINGOESHERE##/$DOMAIN/g" {} \;
-aws route53 change-resource-record-sets --hosted-zone-id $ZONEID --change-batch file://r53.json 
+BUCKETC1=docaasc1
+find r53c1.json -type f -exec sed -i -e "s/##TARGETGOESHERE##/https:\/\/$BUCKETC1.s3-website-us-west-2.amazonaws.com/g" {} \;
+
