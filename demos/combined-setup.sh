14307d92087e3eb2a19fcf934aec3ac8413cff0e
Estaba <grestaba@9801a79671c3.ant.amazon.com>
2018-10-22 15:24:59 +1100

    Test

--- demos/combined-setup.sh
+++ demos/combined-setup.sh
@@ -26,6 +26,8 @@ npm install
 npm run-script build
 cd build/static/js
 find ./ -type f -exec sed -i -e "s/##CNAMEGOESHERE##/$CNAMEC1/g" {} \;
+find ./ -type f -exec sed -i -e "s/##CUSTOMERGOESHERE##/customer1/g" {} \;
+find ./ -type f -exec sed -i -e "s/##DOMAINGOESHERE##/$DOMAIN/g" {} \;
 cd ../../../
 cp -vr build/ ../../monoliths/customer1/app/
 
