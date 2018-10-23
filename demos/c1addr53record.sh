ddb65b762539e62bbcf8395cd22c426022898f5d
Estaba <grestaba@9801a79671c3.ant.amazon.com>
2018-10-23 16:40:37 +1100

    fix r53

--- demos/c1addr53record.sh
+++ demos/c1addr53record.sh
@@ -12,7 +12,7 @@ echo "Adding record to R53"
 ZONEID=`aws route53 list-hosted-zones-by-name --dns-name $DOMAIN | jq --raw-output '.HostedZones[0].Id'`
 echo "ZONEID is $ZONEID"
 cp r53c1.json r53c1-mod.json
-find r53c1-mod.json -type f -exec sed -i -e "s/##TARGETGOESHERE##/https:\/\/$CNAMEC1/g" {} \;
+find r53c1-mod.json -type f -exec sed -i -e "s/##TARGETGOESHERE##/$CNAMEC1/g" {} \;
 find r53c1-mod.json -type f -exec sed -i -e "s/##DOMAINGOESHERE##/$DOMAIN/g" {} \;
 aws route53 change-resource-record-sets --hosted-zone-id $ZONEID --change-batch file://r53c1-mod.json
 rm -f r53c1-mod.json r53c1-mod.json-e 
