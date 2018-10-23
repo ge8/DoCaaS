3378d9a7a375a4563cd9b0e166d275f3b1bec16d
Estaba <grestaba@9801a79671c3.ant.amazon.com>
2018-10-24 10:37:22 +1100

    Deploy App change

--- demos/app1setup.sh
+++ demos/app1setup.sh
@@ -5,10 +5,9 @@ BUCKETC2=docaasc2
 DOMAIN=estaba.net
 
 CNAMEC1=`aws elasticbeanstalk describe-environments --environment-names docaas-customer1-eb-env --no-include-deleted | jq --raw-output '.Environments[0].CNAME'`
+CERTARN=`aws acm list-certificates | jq '.CertificateSummaryList [] | select(.DomainName=='$DOMAIN') .CertificateArn'`
 
 cd ../monoliths/customer1
-
-CERTARN=`aws acm request-certificate --domain-name estaba.net --subject-alternative-names *.estaba.net --validation-method DNS | jq --raw-output '.CertificateArn'`
 cd .ebextensions/
 find securelistener-clb.config -type f -exec sed -i -e "s,##CERTARNGOESHERE##,$CERTARN,g" {} \;
 rm -f securelistener-clb.config-e
