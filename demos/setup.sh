3e9b2eec318fedd9e164d8d72325d4284988bf33
Estaba <grestaba@9801a79671c3.ant.amazon.com>
2018-10-23 16:38:25 +1100

    Add all TODOs - everything working!

--- demos/setup.sh
+++ demos/setup.sh
@@ -31,21 +31,21 @@ echo 'Start App1 setup'
 ./app1setup.sh
 echo 'Finished App1 setup'
 
-
-
 # Add Customer 1 R53 record
 echo 'Start C1 R53 add'
 ./c1addr53record.sh
 echo 'Finished C1 R53 add'
 
-# App 2 Setup
-# Monolith 2 Setup
-# Add Customer 2 R53 record
-# Add DynamoDB
-# Populate DynamoDB
+# App 2 Setup TODO
+# Monolith 2 Setup TODO
+# Add Customer 2 R53 record TODO
+# Add DynamoDB TODO
+# Populate DynamoDB TODO
 
 # Validate ACM is validated
 CERTARN=`aws acm request-certificate --domain-name estaba.net --subject-alternative-names *.estaba.net --validation-method DNS | jq --raw-output '.CertificateArn'`
 echo $CERTARN
 aws acm wait certificate-validated --certificate-arn $CERTARN
 echo 'Validation Done!'
+
+# Add ACM certificate to both load balancers! TODO
