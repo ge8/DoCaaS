f83e2e8d110b4d6503eda3fb319c871591218eef
Estaba <grestaba@9801a79671c3.ant.amazon.com>
2018-10-23 23:14:08 +1100

    Fix sed?

--- demos/monolith1setup.sh
+++ demos/monolith1setup.sh
@@ -6,7 +6,7 @@ cd ../monoliths/customer1
 eb init --platform node.js --region us-west-2
 CERTARN=`aws acm request-certificate --domain-name estaba.net --subject-alternative-names *.estaba.net --validation-method DNS | jq --raw-output '.CertificateArn'`
 cd .ebextensions/
-find securelistener-clb.config  -type f -exec sed -i -e "s/##CERTARNGOESHERE##/$CERTARN/g" {} \;
+find securelistener-clb.config -type f -exec sed -i -e "s,##CERTARNGOESHERE##,$CERTARN,g" {} \;
 rm -f securelistener-clb.config-e
 cd ..
 eb create docaas-customer1-eb-env 
