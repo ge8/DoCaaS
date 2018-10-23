62421b5be728984f924df3a7e56667da5bb198ae
Estaba <grestaba@9801a79671c3.ant.amazon.com>
2018-10-23 22:44:04 +1100

    asd

--- demos/monolith1setup.sh
+++ demos/monolith1setup.sh
@@ -5,8 +5,10 @@
 cd ../monoliths/customer1
 eb init --platform node.js --region us-west-2
 CERTARN=`aws acm request-certificate --domain-name estaba.net --subject-alternative-names *.estaba.net --validation-method DNS | jq --raw-output '.CertificateArn'`
-find .ebextensions/securelistener-clb.config  -type f -exec sed -i -e "s/##CERTARNGOESHERE##/$CERTARN/g" {} \;
-rm -f .ebextensions/securelistener-clb.config-e
+cd .ebextensions/
+find securelistener-clb.config  -type f -exec sed -i -e "s/##CERTARNGOESHERE##/$CERTARN/g" {} \;
+rm -f securelistener-clb.config-e
+cd ..
 eb create docaas-customer1-eb-env 
 aws elasticbeanstalk update-environment --environment-name docaas-customer1-eb-env --option-settings "OptionName=NodeVersion, Namespace=aws:elasticbeanstalk:container:nodejs, Value=8.11.4"
 CNAMEC1=`aws elasticbeanstalk describe-environments --environment-names docaas-customer1-eb-env --no-include-deleted | jq --raw-output '.Environments[0].CNAME'`
