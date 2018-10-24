bc9287570004510d6bbcb46d702598b436eef157
Estaba <grestaba@9801a79671c3.ant.amazon.com>
2018-10-24 14:11:35 +1100

    dynamo populate script

--- demos/dynamo1setup.sh
+++ demos/dynamo1setup.sh
@@ -2,6 +2,8 @@
 BUCKET=docaas
 
 cd ../monoliths/customer1/dynamo/
-sam package --template-file dyanmodb.yaml --s3-bucket $ARTIFACT_BUCKET --output-template-file packaged-dynamo.yaml
+sam package --template-file dynamodb.yaml --s3-bucket $BUCKET --output-template-file packaged-dynamo.yaml
 sam deploy --template-file packaged-dynamo.yaml --stack-name dynamo-customer1 --capabilities CAPABILITY_IAM
+rm -f packaged-dynamo.yaml
+aws dynamodb put-item --table-name data-customer1 --item file://item.json
 cd ../../../demos
