67ad7d6b11bbaaf5164acc99b4b401b86878d71d
Estaba <grestaba@9801a79671c3.ant.amazon.com>
2018-10-24 16:34:39 +1100

    Dynamo PK update

--- monoliths/customer1/core/dataAccess.js
+++ monoliths/customer1/core/dataAccess.js
@@ -37,23 +37,31 @@ exports.checkuser = async (tenantId, username, password) => {
     var params = {
         TableName: 'data-' + tenantId,
         Key: {
-            'key': { S: "user" }
+            'partition-key': { S: 'user' }
         }
     };
+
+    console.log ("DB Params - Key :" + params.Key["partition-key"].S)
+    console.log ("DB Params - TableName :" + params.TableName)
 
     // Retrieve Deck from DDB
     return ddb.getItem(params).promise()
             .then(data =>{
                 let dbUsername = data.Item.username.S;
                 let dbPassword = data.Item.password.S;
+                console.log("ddbusername is:" + dbUsername);
+                console.log("ddbpassword is:" + dbPassword);
                 if (dbUsername.toLowerCase() === username.toLowerCase()
                     && dbPassword === password ) {
+                        console.log("Exiting at 1")
                     return true;
                 } else {
+                    console.log("Exiting at 2")
                     return false;
                 }
             })
             .catch(err => {
+                console.log("Error: " + err)
                 if (err.code === "ResourceNotFoundException") {
                     // Item doesn't exist!
                     return false;   // No Username/Password in the database
@@ -65,7 +73,7 @@ exports.getDeck = async (tenantId, deckId) => {
     var params = {
         TableName: 'data-' + tenantId,
         Key: {
-            'key': { S: "deck-" + deckId }
+            'partition-key': { S: "deck-" + deckId }
         }
     };
 
@@ -100,7 +108,7 @@ exports.getScores = async (tenantId, deckId) => {
     var params = {
         TableName: 'data-' + tenantId,
         Key: {
-            'key': { S: "game-" + deckId }
+            'partition-key': { S: "game-" + deckId }
         }
     };
 
@@ -125,7 +133,7 @@ exports.getScores = async (tenantId, deckId) => {
 
 exports.saveScores = async (tenantId, deckId, scores) => {
     let item = {
-        'key': { S: "game-" + deckId },
+        'partition-key': { S: "game-" + deckId },
         deck: { S: deckId },
         scores: { L: [ ] }
     };
