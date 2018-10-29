2917c0706a52d3ab5649cf74fd07badff1375955
Estaba <grestaba@9801a79671c3.ant.amazon.com>
2018-10-29 17:06:04 +1100

    CNAME script fix

--- demos/monolith1setup.sh
+++ demos/monolith1setup.sh
@@ -4,7 +4,12 @@
 
 cd ../monoliths/customer1
 eb init --platform node.js --region us-west-2
+
+CERTARN='no value'
+CNAMEC1=`aws elasticbeanstalk describe-environments --environment-names docaas-customer1-eb-env --no-include-deleted | jq --raw-output '.Environments[0].CNAME'`
 CERTARN=`aws acm list-certificates | jq --arg DOMAIN "$DOMAIN" '.CertificateSummaryList [] | select(.DomainName==$DOMAIN) .CertificateArn'`
+echo $CERTARN
+
 cd .ebextensions/
 find securelistener-clb.config -type f -exec sed -i -e "s,##CERTARNGOESHERE##,$CERTARN,g" {} \;
 rm -f securelistener-clb.config-e
