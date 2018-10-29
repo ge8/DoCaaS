bf5e10abed8ec77c05b2566ec932660212448401
Estaba <grestaba@9801a79671c3.ant.amazon.com>
2018-10-30 10:26:12 +1100

    Modify src as well

--- demos/app1setup.sh
+++ demos/app1setup.sh
@@ -17,12 +17,17 @@ cd ../../../demos
 cd ../front-end/customer1
 npm install
 npm run-script build
-cd build/static/js
-# find ./ -type f -exec sed -i -e "s/##CNAMEGOESHERE##/$CNAMEC1/g" {} \;
+cd build/static/js #modify build
 find ./ -type f -exec sed -i -e "s/CUSTOMERGOESHERE/customer1/g" {} \;
 find ./ -type f -exec sed -i -e "s/DOMAINGOESHERE.com/$DOMAIN/g" {} \;
 cd ../../../
 cp -vr build/ ../../monoliths/customer1/app/
+
+cd src/components/MainBody #modify source
+find ./ -type f -exec sed -i -e "s/CUSTOMERGOESHERE/customer1/g" {} \;
+find ./ -type f -exec sed -i -e "s/DOMAINGOESHERE.com/$DOMAIN/g" {} \;
+cd ../../../
+
 cd ../../monoliths/customer1
 eb deploy docaas-customer1-eb-env
 cd ../../demos
