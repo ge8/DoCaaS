9d523e1979d6ad3e6200bae118c05e29ad3dce6d
Estaba <grestaba@9801a79671c3.ant.amazon.com>
2018-11-02 15:14:06 +1100

    modify new config with cert

--- demos/monolith1setup.sh
+++ demos/monolith1setup.sh
@@ -10,8 +10,11 @@ CERTARN=`aws acm list-certificates | jq --arg DOMAIN "$DOMAIN" '.CertificateSumm
 echo $CERTARN
 
 cd .ebextensions/
+find healthcheckurl.config -type f -exec sed -i -e "s,CERTARNGOESHERE,$CERTARN,g" {} \;
+rm -f healthcheckurl.config-e
 find securelistener-clb.config -type f -exec sed -i -e "s,CERTARNGOESHERE,$CERTARN,g" {} \;
 rm -f securelistener-clb.config-e
+
 cd ..
 eb create docaas-customer1-eb-env --elb-type application
 cd ../../demos
