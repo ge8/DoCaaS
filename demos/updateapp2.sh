9166d5e3d6273f9c103556448bd83f71cd403361
Estaba <grestaba@9801a79671c3.ant.amazon.com>
2018-11-07 09:56:50 +1100

    Fix certificate for updates

--- demos/updateapp2.sh
+++ demos/updateapp2.sh
@@ -3,24 +3,24 @@
 . ./loadvariables.sh
 
 # Install dependencies
-cd ../front-end/customer1
+cd ../front-end/customer2
 npm install --save aws-amplify 
 npm install --save aws-amplify-react #takes 1 min
 npm install
 
 # Modify mainURL in source
 cd src/components/MainBody #modify source
-find ./ -type f -exec sed -i -e "s/CUSTOMERGOESHERE/customer1/g" {} \;
+find ./ -type f -exec sed -i -e "s/CUSTOMERGOESHERE/customer2/g" {} \;
 find ./ -type f -exec sed -i -e "s/DOMAINGOESHERE.com/$DOMAIN/g" {} \;
 cd ../../../
 
 # Build and Drop Build into Monolith Deployable
 npm run-script build
-cp -vr build/ ../../monoliths/customer1/app/
+cp -vr build/ ../../monoliths/customer2/app/
 
 
 # Deploy & Update BEanstalk env
-cd ../../monoliths/customer1/
+cd ../../monoliths/customer2/
 
 CERTARN='no value'
 CERTARN=`aws acm list-certificates | jq --arg DOMAIN "$DOMAIN" '.CertificateSummaryList [] | select(.DomainName==$DOMAIN) .CertificateArn'`
@@ -29,7 +29,7 @@ cd .ebextensions/
 find alb-secure-listener.config -type f -exec sed -i -e "s,CERTARNGOESHERE,$CERTARN,g" {} \;
 rm -f alb-secure-listener.config-e
 cd ..
-eb deploy docaas-customer1-eb-env
+eb deploy docaas-customer2-eb-env
 cd ../../demos
 
 
