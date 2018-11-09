184eacbdf28d9a0e192e6a824c1d2651b635d682
Estaba <grestaba@9801a79671c3.ant.amazon.com>
2018-11-09 17:23:46 +1100

    Script struggle

--- migration/data-migration.js
+++ migration/data-migration.js
@@ -7,42 +7,77 @@ var cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider({reg
 var USER_POOL_ID = process.env.USERPOOLID
 
 // Get <sub> for customer1. Parameters: customer1 & COGNITOPOOLID 
-var params = {
+var params1 = {
     UserPoolId: USER_POOL_ID, /* required */
     Username: 'customer1' /* required */
     };
-    cognitoidentityserviceprovider.adminGetUser(params, function(err, data) {
-        if (err) console.log(err, err.stack); // an error occurred
-        else {
-            data.UserAttributes.forEach(function (element) {
-                if (element.Name === "sub") {
-                    var C1SUB = element.Value;
-                    console.log(C1SUB);
-                }
-            })
-        }
-    });
+cognitoidentityserviceprovider.adminGetUser(params1, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else {
+        data.UserAttributes.forEach(function (element) {
+            if (element.Name === "sub") {
+                var C1SUB = element.Value;
+                console.log(C1SUB);
+            }
+        })
+    }
+}).promise();
 
-// Get <sub> for customer2. Parameters: customer1 & COGNITOPOOLID 
-var params = {
+// Get <sub> for customer2. Parameters: customer2 & COGNITOPOOLID 
+var params2 = {
     UserPoolId: USER_POOL_ID, /* required */
     Username: 'customer2' /* required */
-    };
-    cognitoidentityserviceprovider.adminGetUser(params, function(err, data) {
-        if (err) console.log(err, err.stack); // an error occurred
-        else {
-            data.UserAttributes.forEach(function (element) {
-                if (element.Name === "sub") {
-                    var C2SUB = element.Value;
-                    console.log(C2SUB);
-                }
-            })
-        }
-    });
+};
+cognitoidentityserviceprovider.adminGetUser(params2, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else {
+        data.UserAttributes.forEach(function (element) {
+            if (element.Name === "sub") {
+                var C2SUB = element.Value;
+                console.log(C2SUB);
+            }
+        })
+    }
+}).promise();
 
 // Scan customer1 DB
+var paramsscan = {
+    TableName: "data-customer1",
+};
+
+dynamodb.scan(paramsscan, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else {
+        console.log(data);           // successful response
+        data.Items.forEach( function (element) {
+            if (element.id.stringify().slice(0,3) === "deck") {
+                DECKID = element.id.stringify().replace("deck-","");
+                console.log("The Deck ID is" + DECKID);
+            }
+            
+        });
+    }
+});
+
+
 
 // Iterate over Items, modify them into two Items (Decks, Games).
+
+// var params = {
+//     Item: {
+//         "id": { S: C1SUB + DECKID}
+//       }
+//     }, 
+//     TableName: "decks-master"
+//    };
+//    dynamodb.putItem(params, function(err, data) {
+//      if (err) console.log(err, err.stack); // an error occurred
+//      else {
+//         console.log(data);           // successful response
+
+//      }
+//    });
+
 
 // Put into Decks and Games
 
