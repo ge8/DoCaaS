f11af6719e3bbe582cb88c03cba19a421a81dffb
Estaba <grestaba@9801a79671c3.ant.amazon.com>
2018-11-11 13:14:42 +1100

    Script finished!

--- migration/data-migration.js
+++ migration/data-migration.js
@@ -7,7 +7,7 @@ let cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider({reg
 let USER_POOL_ID = process.env.USERPOOLID
 
 
-// Get <sub> for customer1. Parameters: customer1 & COGNITOPOOLID 
+// Get <sub> for customer1.
 let params = {
     UserPoolId: USER_POOL_ID, /* required */
     Username: 'customer1' /* required */
@@ -21,26 +21,18 @@ GetUserSub.then(function(data) {
             console.log("C1SUB is: " + C1SUB);
         }
     })
-
-    // Put Customer 1 C1DECKS and C2DECKS into Decks and Games
+    // Scan customer-1 DB
     let params = {
         TableName: "data-customer1"
     };
     let ScanDynamo = dynamodb.scan(params).promise();
-    ScanDynamo.then(function(data) {
-    
-        console.log(data.Items);           // successful response
-    
+    ScanDynamo.then(function(data) {    
         data.Items.forEach( function (item) {
-            // Check for Decks
+            // Check for customer 1 Decks
             if (item.id.S.substring(0,4) === "deck") {
-                console.log("Found: " + item.id.S.slice(4));
+                // Replace "deck" for <sub> and write customer 1 decks to decks-master
                 let ID = { S: C1SUB + item.id.S.slice(4) };
                 let CARDS = { L: item.cards.L };
-                console.log( ID );
-                console.log( CARDS );
-                console.log({"id": ID, "cards": CARDS});
-                // Write to decks-master
                 let params = {
                     TableName: "decks-master",
                     Item: {"id": ID, "cards": CARDS},
@@ -48,29 +40,91 @@ GetUserSub.then(function(data) {
                 };
                 let PutToMaster = dynamodb.putItem(params).promise();
                 PutToMaster.then(function(data) {
-                    console.log ("Successful item written to decks-master: " + data);
+                    console.log ("Customer 1 item written to decks-master: " + ID.S);
                 }).catch(function(err) {
                     console.log(err);
                 });
-            
-
-
-                // Check for Games for this Deck.
-
-
-            } 
-            
-            
-            
+                // If scores exits, write customer 1 scores to games-master
+                if (item.scores) {
+                    let SCORES = { L: item.scores.L };
+                    let params = {
+                        TableName: "games-master",
+                        Item: {"id": ID, "scores": SCORES},
+                        ReturnConsumedCapacity: "TOTAL",
+                    };
+                    let PutToMaster = dynamodb.putItem(params).promise();
+                    PutToMaster.then(function(data) {
+                        console.log ("Customer 1 item written to games-master: " + ID.S);
+                    }).catch(function(err) {
+                        console.log(err);
+                    });
+                }
+            }       
         });
-    
     }).catch(function(err) {
         console.log(err);
     });
-    
-
-
 }).catch(function(err) {
     console.log(err);
 });
 
+// Get <sub> for customer2.
+params = {
+    UserPoolId: USER_POOL_ID, /* required */
+    Username: 'customer2' /* required */
+};
+GetUserSub = cognitoidentityserviceprovider.adminGetUser(params).promise();
+GetUserSub.then(function(data) {
+    let C2SUB = "";
+    data.UserAttributes.forEach(function (element) {
+        if (element.Name === "sub") {
+            C2SUB = element.Value;
+            console.log("C2SUB is: " + C2SUB);
+        }
+    })
+    // Scan customer-2 DB
+    let params = {
+        TableName: "data-customer2"
+    };
+    let ScanDynamo = dynamodb.scan(params).promise();
+    ScanDynamo.then(function(data) {    
+        data.Items.forEach( function (item) {
+            // Check for customer 2 Decks
+            if (item.id.S.substring(0,4) === "deck") {
+                // Replace "deck" for <sub> and write customer 2 decks to decks-master
+                let ID = { S: C2SUB + item.id.S.slice(4) };
+                let CARDS = { L: item.cards.L };
+                let params = {
+                    TableName: "decks-master",
+                    Item: {"id": ID, "cards": CARDS},
+                    ReturnConsumedCapacity: "TOTAL",
+                };
+                let PutToMaster = dynamodb.putItem(params).promise();
+                PutToMaster.then(function(data) {
+                    console.log ("Customer 2 item written to decks-master: " + ID.S);
+                }).catch(function(err) {
+                    console.log(err);
+                });
+                // If scores exits, write customer 2 scores to games-master
+                if (item.scores) {
+                    let SCORES = { L: item.scores.L };
+                    let params = {
+                        TableName: "games-master",
+                        Item: {"id": ID, "scores": SCORES},
+                        ReturnConsumedCapacity: "TOTAL",
+                    };
+                    let PutToMaster = dynamodb.putItem(params).promise();
+                    PutToMaster.then(function(data) {
+                        console.log ("Customer 2 item written to games-master: " + ID.S);
+                    }).catch(function(err) {
+                        console.log(err);
+                    });
+                }
+            }       
+        });
+    }).catch(function(err) {
+        console.log(err);
+    });
+}).catch(function(err) {
+    console.log(err);
+});
