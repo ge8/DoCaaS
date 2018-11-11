a0faa39bfc6aede09616383b6f0f13a0464c5c3c
Estaba <grestaba@9801a79671c3.ant.amazon.com>
2018-11-11 18:07:44 +1100

    reset-presentation script

--- demos/reset-presentation.sh
+++ demos/reset-presentation.sh
@@ -3,17 +3,60 @@
 . ./loadvariables.sh
 
 # manually discard all changes in local repo (deletes all new local files and changes)
+cd ../
+git reset --hard HEAD
+git clean -f
+cd demos/
 
-# delete Amplify Stack
-
-# redeploy App1
-
-# redeploy App2
+# delete Amplify Stack TODOOOOOOOO
+aws cloudformation delete-stack --stack-name customer1-20181106124851
 
 # Delete docaas SAM stack 
+aws cloudformation delete-stack --stack-name docaas
 
 # Delete docaas-dynamos stack
+aws cloudformation delete-stack --stack-name docaas-dynamos
+
+# redeploy App1
+./updateapp1.sh
+
+# redeploy App2
+./updateapp2.sh
+
+# Modify R53 CNAME for customer1.xxx back to ElasticBeanstalk
+CNAMEC1=`aws elasticbeanstalk describe-environments --environment-names docaas-customer1-eb-env --no-include-deleted | jq --raw-output '.Environments[0].CNAME'`
+echo "Updating C1 CNAME record on R53"
+ZONEID=`aws route53 list-hosted-zones-by-name --dns-name $DOMAIN | jq --raw-output '.HostedZones[0].Id'`
+echo "ZONEID is $ZONEID"
+cp r53c1.json r53c1-mod.json
+find r53c1-mod.json -type f -exec sed -i -e "s/##TARGETGOESHERE##/$CNAMEC1/g" {} \;
+find r53c1-mod.json -type f -exec sed -i -e "s/##DOMAINGOESHERE##/$DOMAIN/g" {} \;
+aws route53 change-resource-record-sets --hosted-zone-id $ZONEID --change-batch file://r53c1-mod.json
+rm -f r53c1-mod.json r53c1-mod.json-e 
+echo "C1 CNAME Record Updated from R53"
+
+CNAMEC2=`aws elasticbeanstalk describe-environments --environment-names docaas-customer2-eb-env --no-include-deleted | jq --raw-output '.Environments[0].CNAME'`
+echo "Updating C2 CNAME record on R53"
+ZONEID=`aws route53 list-hosted-zones-by-name --dns-name $DOMAIN | jq --raw-output '.HostedZones[0].Id'`
+echo "ZONEID is $ZONEID"
+cp r53c2.json r53c2-mod.json
+find r53c2-mod.json -type f -exec sed -i -e "s/##TARGETGOESHERE##/$CNAMEC2/g" {} \;
+find r53c2-mod.json -type f -exec sed -i -e "s/##DOMAINGOESHERE##/$DOMAIN/g" {} \;
+aws route53 change-resource-record-sets --hosted-zone-id $ZONEID --change-batch file://r53c2-mod.json
+rm -f r53c2-mod.json r53c2-mod.json-e 
+echo "C2 CNAME Record Updated from R53"
+
+# Confirm cfn deleted with Amplify STACK!!
+aws cloudformation wait stack-delete-complete --stack-name docaas-dynamos
+aws cloudformation wait stack-delete-complete --stack-name docaas
+aws cloudformation wait stack-delete-complete --stack-name customer1-20181106124851
 
 # delete IAM policies DefaultPolicyForAuthenticatedUsers and DynamoPolicyForAuthenticatedUsers
+AWSACCOUNT=`aws sts get-caller-identity --output text --query 'Account'`
+echo "The AWS Account is: $AWSACCOUNT"
+aws iam delete-policy --policy-arn arn:aws:iam::$AWSACCOUNT:policy/DoCaaSDynamoPolicyForAuthenticated
+aws iam delete-policy --policy-arn arn:aws:iam::$AWSACCOUNT:policy/DoCaaSDefaultPolicyForAuthenticated
 
-# DNS change to redirect back to customer1.xxx from xxx.com 
+
+
+
--- lambdas/deploy-adam.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-REGION=ap-southeast-2
-BUCKET=adams-s3
-
-sam package --template-file docaastemplate.yaml --s3-bucket $BUCKET --output-template-file packaged.yaml --region $REGION
-sam deploy --template-file packaged.yaml --stack-name docaas-cut-the-deck --capabilities CAPABILITY_IAM --region $REGION
-
-rm -f packaged.yaml
