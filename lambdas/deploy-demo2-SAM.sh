fc50677b4c5b7b51a50643272d1d805d769ca6ec
Estaba <grestaba@9801a79671c3.ant.amazon.com>
2018-11-11 13:29:27 +1100

    Split SAM templates and deploy scripts for demos

--- lambdas/deploy-demo2-SAM.sh
+++ lambdas/deploy-demo2-SAM.sh
@@ -6,7 +6,7 @@ BUCKET=docaas
 aws cloudformation delete-stack --stack-name docaas
 aws cloudformation wait stack-delete-complete --stack-name docaas
 
-sam package --template-file scaffolding-template.yaml --s3-bucket $BUCKET --output-template-file packaged.yaml --region $REGION
+sam package --template-file demo2-template.yaml --s3-bucket $BUCKET --output-template-file packaged.yaml --region $REGION
 sam deploy --template-file packaged.yaml --stack-name docaas --capabilities CAPABILITY_NAMED_IAM --region $REGION
 
 rm -f packaged.yaml
