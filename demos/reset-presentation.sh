11a17e5893d00ff9384655b2d15711c55964632e
Estaba <grestaba@9801a79671c3.ant.amazon.com>
2018-11-11 13:35:34 +1100

    Reset presentation instructions ready!

---
+++ demos/reset-presentation.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+. ./loadvariables.sh
+
+# manually discard all changes in local repo (deletes all new local files and changes)
+
+# delete Amplify Stack
+
+# redeploy App1
+
+# redeploy App2
+
+# Delete docaas SAM stack 
+
+# Delete docaas-dynamos stack
+
+# delete IAM policies DefaultPolicyForAuthenticatedUsers and DynamoPolicyForAuthenticatedUsers
+
+# DNS change to redirect back to customer1.xxx from xxx.com 
