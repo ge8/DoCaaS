6c066f1334cd0391fc1909dc7dfe5f1191ebda40
Adam Strickland <adam@Adams-MacBook-Pro.local>
2018-11-14 10:41:05 +1100

    Remove unnecessary cli calls

--- lambdas/deploy-demo4-SAM.sh
+++ lambdas/deploy-demo4-SAM.sh
@@ -3,9 +3,6 @@
 REGION=us-west-2
 BUCKET=docaas
 
-aws cloudformation delete-stack --stack-name docaas
-aws cloudformation wait stack-delete-complete --stack-name docaas
-
 sam package --template-file demo4-template.yaml --s3-bucket $BUCKET --output-template-file packaged.yaml --region $REGION
 sam deploy --template-file packaged.yaml --stack-name docaas --capabilities CAPABILITY_NAMED_IAM --region $REGION
 
