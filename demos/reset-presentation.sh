19846c733e97cb1743c7bd9429ace3df9c6d24aa
Estaba <grestaba@9801a79671c3.ant.amazon.com>
2018-11-14 14:02:04 +1100

    Fix

--- demos/reset-presentation.sh
+++ demos/reset-presentation.sh
@@ -20,6 +20,9 @@ ROLENAMEAUTH=${INPUT#*/}   # remove prefix ending in "/"
 echo $ROLENAMEAUTH
 
 # Attach 2 policies to Authenticated Role
+AWSACCOUNT=`aws sts get-caller-identity --output text --query 'Account'`
+echo "The AWS Account is: $AWSACCOUNT"
+
 aws iam detach-role-policy --role-name $ROLENAMEAUTH --policy-arn arn:aws:iam::$AWSACCOUNT:policy/DoCaaSDynamoPolicyForAuthenticated
 aws iam detach-role-policy --role-name $ROLENAMEAUTH --policy-arn arn:aws:iam::$AWSACCOUNT:policy/DoCaaSDefaultPolicyForAuthenticated
 
@@ -72,8 +75,6 @@ aws cloudformation wait stack-delete-complete --stack-name docaas
 aws cloudformation wait stack-delete-complete --stack-name $STACKNAME
 
 # delete IAM policies DefaultPolicyForAuthenticatedUsers and DynamoPolicyForAuthenticatedUsers
-AWSACCOUNT=`aws sts get-caller-identity --output text --query 'Account'`
-echo "The AWS Account is: $AWSACCOUNT"
 aws iam delete-policy --policy-arn arn:aws:iam::$AWSACCOUNT:policy/DoCaaSDynamoPolicyForAuthenticated
 aws iam delete-policy --policy-arn arn:aws:iam::$AWSACCOUNT:policy/DoCaaSDefaultPolicyForAuthenticated
 
