e84229f97dbf5d582ef6b28e651f657d34808fc5
Estaba <grestaba@9801a79671c3.ant.amazon.com>
2018-11-19 16:42:06 +1100

    update the stage for demo4

--- lambdas/deploy-demo4-SAM.sh
+++ lambdas/deploy-demo4-SAM.sh
@@ -6,4 +6,7 @@ BUCKET=docaas
 sam package --template-file demo4-template.yaml --s3-bucket $BUCKET --output-template-file packaged.yaml --region $REGION
 sam deploy --template-file packaged.yaml --stack-name docaas --capabilities CAPABILITY_NAMED_IAM --region $REGION
 
+RESTAPI=`aws cloudformation describe-stacks --stack-name docaas --query "Stacks[0].Outputs[0].OutputValue" --output text`
+aws apigateway update-stage --rest-api-id $RESTAPI --stage-name Prod
+
 rm -f packaged.yaml
