bec4dd25e7caf3211706d25032ab9fdccb5b2193
Estaba <grestaba@9801a79671c3.ant.amazon.com>
2018-11-19 17:04:27 +1100

    fix APIGW deployment

--- lambdas/deploy-demo2-SAM.sh
+++ lambdas/deploy-demo2-SAM.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-REGION=us-west-2
-BUCKET=docaas
+. ./../demos/loadvariables.sh
 
 sam package --template-file demo2-template.yaml --s3-bucket $BUCKET --output-template-file packaged.yaml --region $REGION
 sam deploy --template-file packaged.yaml --stack-name docaas --capabilities CAPABILITY_NAMED_IAM --region $REGION
--- lambdas/deploy-demo4-SAM.sh
+++ lambdas/deploy-demo4-SAM.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-REGION=us-west-2
-BUCKET=docaas
+. ./../demos/loadvariables.sh
 
 sam package --template-file demo4-template.yaml --s3-bucket $BUCKET --output-template-file packaged.yaml --region $REGION
 sam deploy --template-file packaged.yaml --stack-name docaas --capabilities CAPABILITY_NAMED_IAM --region $REGION
 
 RESTAPI=`aws cloudformation describe-stacks --stack-name docaas --query "Stacks[0].Outputs[0].OutputValue" --output text`
-aws apigateway update-stage --rest-api-id $RESTAPI --stage-name Prod
+DEPID=`aws apigateway create-deployment --rest-api-id $RESTAPI --stage-name Prod --query "id" --output text`
+aws apigateway update-stage --rest-api-id $RESTAPI --stage-name Prod --patch-operations op='replace',path='/deploymentId',value=$DEPID
 
 rm -f packaged.yaml
