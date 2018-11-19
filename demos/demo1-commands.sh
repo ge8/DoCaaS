c2051f287c7688c61e512545e367d3a0969180b6
Estaba <grestaba@9801a79671c3.ant.amazon.com>
2018-11-20 10:12:15 +1100

    npm install before

--- demos/demo1-commands.sh
+++ demos/demo1-commands.sh
@@ -12,6 +12,7 @@
 
 # New multi-tenant app
 rsync -ax --exclude 'node_modules' front-end/customer1 ../docaas
+cd multi-tenant-app && npm install
 
 
 
@@ -31,7 +32,6 @@ amplify init
 
 amplify add auth 
 amplify push
-npm install
 
 
 
