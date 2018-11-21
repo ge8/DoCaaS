efc351d39aa9dd6f3624e442ec4d6c7dd19d50b0
Estaba <grestaba@9801a79671c3.ant.amazon.com>
2018-11-21 19:20:06 +1100

    Warm up for lambdas

--- lambdas/deploy-demo3-SAM.sh
+++ lambdas/deploy-demo3-SAM.sh
@@ -9,11 +9,17 @@ RESTAPI=`aws cloudformation describe-stacks --stack-name docaas --query "Stacks[
 DEPID=`aws apigateway create-deployment --rest-api-id $RESTAPI --stage-name Prod --query "id" --output text`
 aws apigateway update-stage --rest-api-id $RESTAPI --stage-name Prod --patch-operations op='replace',path='/deploymentId',value=$DEPID
 
-# nohup aws lambda invoke Authorizer &
-# nohup aws lambda invoke CORS &
-# nohup aws lambda invoke Get &
-# nohup aws lambda invoke Create &
-# nohup aws lambda invoke Shuffle &
-# nohup aws lambda invoke Game &
+FUNCTION0=`aws lambda list-functions --query "Functions[0].FunctionName" --output text`
+aws lambda invoke --function-name $FUNCTION0 delete.json >> delete2.json &
+FUNCTION1=`aws lambda list-functions --query "Functions[1].FunctionName" --output text`
+aws lambda invoke --function-name $FUNCTION1 delete.json >> delete2.json &
+FUNCTION2=`aws lambda list-functions --query "Functions[2].FunctionName" --output text`
+aws lambda invoke --function-name $FUNCTION2 delete.json >> delete2.json &
+FUNCTION3=`aws lambda list-functions --query "Functions[3].FunctionName" --output text`
+aws lambda invoke --function-name $FUNCTION3 delete.json >> delete2.json &
+FUNCTION4=`aws lambda list-functions --query "Functions[4].FunctionName" --output text`
+aws lambda invoke --function-name $FUNCTION4 delete.json >> delete2.json &
+FUNCTION5=`aws lambda list-functions --query "Functions[5].FunctionName" --output text`
+aws lambda invoke --function-name $FUNCTION5 delete.json >> delete2.json
 
-rm -f packaged.yaml
+rm -f packaged.yaml delete.json delete2.json
