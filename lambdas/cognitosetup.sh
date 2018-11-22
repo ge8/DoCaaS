d7c6889812d9692ba4905df31f52cd8db4e8e81e
Estaba <grestaba@9801a79671c3.ant.amazon.com>
2018-11-23 10:58:04 +1100

    Remove Cognito Clients

--- lambdas/cognitosetup.sh
+++ lambdas/cognitosetup.sh
@@ -19,16 +19,11 @@ echo "The Authenticated Role ARN is: $USERPOOLID"
 AWSACCOUNT=`aws sts get-caller-identity --output text --query 'Account'`
 echo "The AWS Account is: $AWSACCOUNT"
 
-#Get Cognito Clients
-CLIENT1=`aws cognito-idp list-user-pool-clients --user-pool-id $USERPOOLID --max-results 50 --query 'UserPoolClients[0].ClientId' --output text`
-echo "The Cognito Client1 is: $CLIENT1"
-CLIENT2=`aws cognito-idp list-user-pool-clients --user-pool-id $USERPOOLID --max-results 50 --query 'UserPoolClients[1].ClientId' --output text`
-echo "The Cognito Client2 is: $CLIENT2"
-
 # Get RoleNameAuth
 INPUT=`aws cognito-identity get-identity-pool-roles --identity-pool-id $IDENTITYPOOLID --query "Roles.authenticated" --output text`
 ROLENAMEAUTH=${INPUT#*/}   # remove prefix ending in "/"
 echo $ROLENAMEAUTH
+
 
 
 # Attach 2 policies to Authenticated Role
@@ -37,10 +32,6 @@ aws iam attach-role-policy --role-name $ROLENAMEAUTH --policy-arn arn:aws:iam::$
 
 # Create Cognito attribute "plan"
 aws cognito-idp add-custom-attributes --user-pool-id $USERPOOLID --custom-attributes Name=plan,AttributeDataType=String
-
-# # Make "plan" attribute readable by clients
-# aws cognito-idp update-user-pool-client --user-pool-id $USERPOOLID --client-id $CLIENT1 --read-attributes "custom:plan"
-# aws cognito-idp update-user-pool-client --user-pool-id $USERPOOLID --client-id $CLIENT2 --read-attributes "custom:plan"
 
 # Add silver plan to customer1
 aws cognito-idp admin-update-user-attributes --user-pool-id $USERPOOLID --username customer1 --user-attributes Name=custom:plan,Value=silver
